910faf035db3039b59bb4403835aacf04916da03
nursultan <@ntorobaev>
2022-05-12 12:14:27 +0600

    Second change

--- src/main/java/company/Application.java
+++ src/main/java/company/Application.java
@@ -3,6 +3,7 @@ package company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
