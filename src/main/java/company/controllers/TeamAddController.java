ec0b3621d2378d0cdc974a7a7ca2b50292b7d81b
nursultan <@ntorobaev>
2022-06-14 10:46:56 +0600

    Teams added

---
+++ src/main/java/company/controllers/TeamAddController.java
@@ -0,0 +1,34 @@
+package company.controllers;
+
+import company.api.Team;
+import company.repos.TeamRepo;
+import io.swagger.models.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/team_add")
+public class TeamAddController {
+    @Autowired
+    private TeamRepo teamRepo;
+
+    @GetMapping
+    public String teamAdd(Model model){
+        return "/team_add";
+    }
+    @PostMapping
+    public String postTeams(HttpServletRequest request, Model model) {
+        Team team = new Team(request.getParameter("teamName"), request.getParameter("departmentName"), (short) 0, (short) 0, (short) 0);
+        team.setDraws((short)0);
+        team.setGoals((short)0);
+        team.setLoses((short)0);
+        team.setWins((short)0);
+        teamRepo.save(team);
+        return "redirect:team";
+    }
+}
