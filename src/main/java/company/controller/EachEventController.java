1389f949f85394114c61ea688fcffbf00517af78
nursultan <@ntorobaev>
2022-09-28 16:26:08 +0600

    28.09.2022

--- src/main/java/company/controller/EachEventController.java
+++ src/main/java/company/controller/EachEventController.java
@@ -1,7 +1,9 @@
 package company.controller;
 
 import company.entity.Consumer;
+import company.entity.Match;
 import company.entity.Tournament;
+import company.repos.MatchRepo;
 import company.repos.TournamentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,11 +13,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
 public class EachEventController {
+
+    @Autowired
+    private MatchRepo matchRepo;
     @Autowired
     private TournamentRepo tournamentRepo;
     @GetMapping("/eachEvent/{id}")
@@ -25,6 +32,14 @@ public class EachEventController {
         Optional<Tournament> tournament = tournamentRepo.findById(id);
         model.addAttribute("selectedEvent", tournament);
         model.addAttribute("teams", tournament.get().getTeams());
+        List<Match> mats = (List<Match>) matchRepo.findAll();
+        List<Match> matches = new ArrayList<>();
+        for(Match match : mats){
+            if(match.getTournament().getId() == id){
+                matches.add(match);
+            }
+        }
+        model.addAttribute("matches", matches);
         return "eachEvent";
     }
 }
